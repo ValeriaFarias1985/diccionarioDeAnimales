784b4d9ca7a9151dd822712d8ab0b773a0ed0700
Cyberkrane <cyberkrane@gmail.com>
2021-09-24 10:55:44 -0300

    mejore el footer

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-var animales = [
-    {
+var animales = [{
         imagen: "./imagenes/abeja.jpg",
         nombre: "aveja",
         traduccion: "bee",
@@ -9,331 +8,331 @@ var animales = [
         nombre: "Aguila",
         traduccion: "Eagle",
 
-    },       
+    },
     {
         imagen: "./imagenes/araña.jpg",
         nombre: "Araña",
         traduccion: "spider",
 
-    },       
+    },
     {
         imagen: "./imagenes/ardilla.png",
         nombre: "avispa",
         traduccion: " wasp ",
 
-    },      
+    },
     {
         imagen: "./imagenes/ballena.jpg",
         nombre: "ballena",
         traduccion: " whale ",
 
-    },       
+    },
     {
         imagen: "./imagenes/bisonte (2).jpg",
         nombre: "bisonte",
         traduccion: "bison",
 
-    },      
+    },
     {
         imagen: "./imagenes/bufalo.jpg",
         nombre: "búfalo",
-        traduccion:"buffalo", 
+        traduccion: "buffalo",
 
-    },       
+    },
     {
         imagen: "./imagenes/burro.jpg",
         nombre: "burro ",
-        traduccion: " donkey ", 
+        traduccion: " donkey ",
 
-    },    
+    },
     {
         imagen: "./imagenes/caballo.jpg",
         nombre: "caballo",
-        traduccion: "horse", 
+        traduccion: "horse",
 
-    },       
+    },
     {
         imagen: "./imagenes/camello.jpg",
-        nombre:"camello",
+        nombre: "camello",
         traduccion: " camel ",
 
-    },      
+    },
     {
         imagen: "./imagenes/canario.jpg",
         nombre: "canario ",
-        traduccion:" canary ", 
+        traduccion: " canary ",
 
-    },      
+    },
     {
         imagen: "./imagenes/cangrejo.jpg",
         nombre: "cangrejo ",
         traduccion: " crab ",
 
-    },      
+    },
     {
         imagen: "./imagenes/canguro.jpeg",
         nombre: "canguro",
-        traduccion:"kangaroo", 
+        traduccion: "kangaroo",
 
-    },       
+    },
     {
-        imagen:"./imagenes/caracol.jpg",
+        imagen: "./imagenes/caracol.jpg",
         nombre: "caracol",
-        traduccion: "snail", 
+        traduccion: "snail",
 
-    },       
+    },
     {
         imagen: "./imagenes/cebra.jpg",
         nombre: "cebra",
-        traduccion:" zebra ", 
+        traduccion: " zebra ",
 
-    },       
+    },
     {
-        imagen: "./imagenes/cerdo.jpg" ,
+        imagen: "./imagenes/cerdo.jpg",
         nombre: "cerdo",
-        traduccion: " pig ", 
+        traduccion: " pig ",
 
-    },      
+    },
     {
         imagen: "./imagenes/chimpance.jpg",
         nombre: "chimpancé",
-        traduccion: "chimpanzee", 
+        traduccion: "chimpanzee",
 
-    },       
+    },
     {
         imagen: "./imagenes/ciervo.jpg",
         nombre: "ciervo",
-        traduccion:"deer",  
+        traduccion: "deer",
 
-    },       
+    },
     {
         imagen: "./imagenes/cisne.jpg",
         nombre: "cisne",
-        traduccion:"swan", 
+        traduccion: "swan",
 
-    },       
+    },
     {
         imagen: "./imagenes/cocodrilo.jpg",
         nombre: "cocodrilo ",
-        traduccion: " crocodile ", 
+        traduccion: " crocodile ",
 
-    },      
+    },
     {
         imagen: "./imagenes/conejo.jpg",
         nombre: "conejo",
-        traduccion:"rabbit", 
+        traduccion: "rabbit",
 
-    },       
+    },
     {
         imagen: "./imagenes/elefante.jpg",
         nombre: "elefante",
-        traduccion:"elephant", 
+        traduccion: "elephant",
 
-    },       
+    },
     {
         imagen: "./imagenes/escarabajo.jpg",
         nombre: "escarabajo",
-        traduccion: "beetle", 
+        traduccion: "beetle",
 
-    },       
+    },
     {
         imagen: "./imagenes/escorpion.jpg",
         nombre: "escorpión",
-        traduccion: "scorpion", 
+        traduccion: "scorpion",
 
-    },      
+    },
     {
         imagen: "./imagenes/foca.jpg",
         nombre: "foca",
-        traduccion:" seal ", 
+        traduccion: " seal ",
 
-    },       
+    },
     {
-        imagen:"./imagenes/gallina.jpg", 
+        imagen: "./imagenes/gallina.jpg",
         nombre: "gallina",
         traduccion: " hen ",
 
-    },      
+    },
     {
         imagen: "./imagenes/gallo.jpg",
-        nombre:"gallo",
-        traduccion: " cock ", 
+        nombre: "gallo",
+        traduccion: " cock ",
 
-    },      
+    },
     {
         imagen: "./imagenes/gato.jpg",
         nombre: "gato",
         traduccion: "cat",
 
-    },      
+    },
     {
         imagen: "./imagenes/girafa.jpg",
         nombre: "jirafa",
-        traduccion: "giraffe", 
+        traduccion: "giraffe",
 
-    },       
+    },
     {
         imagen: "./imagenes/golondrina.jpg",
         nombre: "golondrina",
         traduccion: "swallow",
 
-    },       
+    },
     {
         imagen: "./imagenes/hiena.jpg",
         nombre: "hiena",
         traduccion: "hyena",
 
-    },       
+    },
     {
         imagen: "./imagenes/hipopotamo.jpg",
         nombre: "hipopotamo",
         traduccion: "hippopotamus",
 
-    },       
+    },
     {
         imagen: "./imagenes/hormiga.jpg",
         nombre: "hormiga",
         traduccion: "ant",
 
-    },      
+    },
     {
         imagen: "./imagenes/javali.jpg",
         nombre: "javali",
         traduccion: "wild boar",
 
-    },       
+    },
     {
-        imagen: "./imagenes/leon.jpg" ,
+        imagen: "./imagenes/leon.jpg",
         nombre: "leon",
         traduccion: "Lion",
 
-    },       
+    },
     {
         imagen: "./imagenes/leopardo.jpg",
         nombre: "leopardo",
-        traduccion:"leopard", 
+        traduccion: "leopard",
 
-    },       
+    },
     {
         imagen: "./imagenes/loro.jpg",
         nombre: "loro",
         traduccion: " parrot ",
 
-    },       
+    },
     {
         imagen: "./imagenes/mono.jpg",
         nombre: "mono",
         traduccion: "monkey",
 
-    },      
+    },
     {
         imagen: "./imagenes/mosca.jpg",
         nombre: "mosca",
         traduccion: " fly ",
 
-    },       
+    },
     {
         imagen: "./imagenes/obeja.jpg",
         nombre: "obeja",
         traduccion: "sheep",
 
-    },       
+    },
     {
         imagen: "./imagenes/oso panda.jpg",
         nombre: "oso panda",
         traduccion: "panda bear",
 
-    },       
+    },
     {
         imagen: "./imagenes/oso polar.jpg",
         nombre: "oso polar",
         traduccion: "plar bear",
 
-    },       
+    },
     {
         imagen: "./imagenes/oso.jpg",
         nombre: "oso",
         traduccion: "bear",
 
-    },       
+    },
     {
         imagen: "./imagenes/perdiz.jpg",
         nombre: "perdiz",
         traduccion: "partridge",
 
-    },       
+    },
     {
         imagen: "./imagenes/perro.jpg",
         nombre: "perro",
         traduccion: "dog",
 
-    },       
+    },
     {
         imagen: "./imagenes/pez.jpg",
         nombre: "pez",
         traduccion: "fish",
 
-    },       
+    },
     {
         imagen: "./imagenes/pinguino.jpg",
         nombre: "pinguino",
         traduccion: "penguin ",
 
-    },      
+    },
     {
         imagen: "./imagenes/pollo.jpg",
         nombre: "pollo",
         traduccion: "chick",
 
-    },       
+    },
     {
         imagen: "./imagenes/saltamontes.jpg",
         nombre: "saltamontes",
         traduccion: "grasshoppers",
 
-    },       
+    },
     {
         imagen: "./imagenes/serpiente.jpg",
         nombre: "serpiente",
         traduccion: "snake",
 
-    },       
+    },
     {
-        imagen:"./imagenes/tiburon.jpg", 
+        imagen: "./imagenes/tiburon.jpg",
         nombre: "tiburon",
         traduccion: "shark",
 
-    },       
+    },
     {
         imagen: "./imagenes/tigre.jpg",
         nombre: "tigre",
-        traduccion: "tiger", 
+        traduccion: "tiger",
 
-    },       
+    },
     {
         imagen: "./imagenes/topo.jpg",
         nombre: "topo",
         traduccion: "mole",
 
-    },       
+    },
     {
         imagen: "./imagenes/toro.jpg",
         nombre: "toro",
         traduccion: "bull",
 
-    },       
+    },
     {
         imagen: "./imagenes/tortuga.jpg",
         nombre: "tortuga",
         traduccion: "turtle",
 
-    },      
+    },
     {
         imagen: "./imagenes/vaca.jpg",
         nombre: "vaca",
         traduccion: "cow",
 
-    },       
+    },
     {
         imagen: "./imagenes/zorro.jpg",
         nombre: "zorro",
@@ -347,21 +346,21 @@ var animales = [
 
     },
     {
-        imagen: "./imagenes/coballo.jpg" ,
+        imagen: "./imagenes/coballo.jpg",
         nombre: "coballo",
-        traduccion: "coballo" ,
+        traduccion: "coballo",
 
     },
     {
-        imagen: "./imagenes/cuis.jpg" ,
-        nombre:"cuis" ,
-        traduccion:"cuis" ,
+        imagen: "./imagenes/cuis.jpg",
+        nombre: "cuis",
+        traduccion: "cuis",
 
     },
     {
         imagen: "./imagenes/hamster.jpg",
         nombre: "hamster",
-        traduccion:"hamster" ,
+        traduccion: "hamster",
 
     },
     {
@@ -371,22 +370,26 @@ var animales = [
 
     },
     {
-        imagen: "./imagenes/pantera.jpg" ,
-        nombre:"pantera" ,
+        imagen: "./imagenes/pantera.jpg",
+        nombre: "pantera",
         traduccion: "panther",
 
     },
- ];  
- 
- 
+];
 
 
+let botonTraducir = document.querySelector('.button');
+botonTraducir.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log('diste click');
+})
 
- //<div class="tarjeta">
- //<div class="card" style="width: 18rem;">
-   //  <img src="./imagenes/ballena.jpg" class="card-img-top" alt="...">
-    // <div class="card-body">
-     //    <h3>Nombre = Ballena</h3>
-  //   </div>
- //</div>
+
+//<div class="tarjeta">
+//<div class="card" style="width: 18rem;">
+//  <img src="./imagenes/ballena.jpg" class="card-img-top" alt="...">
+// <div class="card-body">
+//    <h3>Nombre = Ballena</h3>
+//   </div>
+//</div>
 //</div>
